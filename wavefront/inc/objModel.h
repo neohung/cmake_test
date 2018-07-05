62e99e24ab6944bd70f1b1d253cfe765c4cb0ed2
neo <neo.hung@quantatw.com>
2018-07-05 14:20:07 +0800

    test

---
+++ wavefront/inc/objModel.h
@@ -0,0 +1,20 @@
+#ifndef   __OBJMODEL_H__
+#define   __OBJMODEL_H__
+
+#include<stdio.h>
+#include<math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* EOF */
+
+
